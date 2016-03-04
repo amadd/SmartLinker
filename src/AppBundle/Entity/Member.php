c905c7821541edef503335669615f5346144f816
Cabridens <cabridens@hotmail.fr>
2016-03-04 16:19:41 +0100

    Added all jointure

--- src/AppBundle/Entity/Member.php
+++ src/AppBundle/Entity/Member.php
@@ -5,30 +5,114 @@ namespace AppBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * Member
- *
- * @ORM\Table(name="member")
- * @ORM\Entity(repositoryClass="AppBundle\Repository\MemberRepository")
+ * @ORM\Entity
+ * @ORM\Table(name="Member")
+ * @ORM\InheritanceType("SINGLE_TABLE")
+ * @ORM\DiscriminatorColumn(name="type", type="string")
+ * @ORM\DiscriminatorMap( {"organization" = "Organization", "contact" = "Contact"} )
  */
 class Member
 {
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    private $id;
+  /**
+  * @var int
+  *
+  * @ORM\Column(name="id", type="integer")
+  * @ORM\Id
+  * @ORM\GeneratedValue(strategy="AUTO")
+  */
+  private $id;
 
+  /**
+  * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Address")
+  */
+  private $address;
+
+  /**
+  * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Phone")
+  */
+  private $phone;
+
+  /**
+  * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Email")
+  */
+  private $email;
+
+  /**
+  * Get id
+  *
+  * @return integer
+  */
+  public function getId()
+  {
+    return $this->id;
+  }
 
     /**
-     * Get id
+     * Set address
      *
-     * @return integer 
+     * @param \AppBundle\Entity\Address $address
+     * @return Member
      */
-    public function getId()
+    public function setAddress(\AppBundle\Entity\Address $address = null)
     {
-        return $this->id;
+        $this->address = $address;
+
+        return $this;
+    }
+
+    /**
+     * Get address
+     *
+     * @return \AppBundle\Entity\Address 
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Set phone
+     *
+     * @param \AppBundle\Entity\Phone $phone
+     * @return Member
+     */
+    public function setPhone(\AppBundle\Entity\Phone $phone = null)
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    /**
+     * Get phone
+     *
+     * @return \AppBundle\Entity\Phone 
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * Set email
+     *
+     * @param \AppBundle\Entity\Email $email
+     * @return Member
+     */
+    public function setEmail(\AppBundle\Entity\Email $email = null)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return \AppBundle\Entity\Email 
+     */
+    public function getEmail()
+    {
+        return $this->email;
     }
 }
